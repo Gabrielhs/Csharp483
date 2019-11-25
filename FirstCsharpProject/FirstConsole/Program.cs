8813dc9b983bff619db80c3f639ca5692f16d7b9
Gabriel Hillesheim da Silva <gabriel.hillesheim@philips.com>
2019-11-25 20:55:50 -0300

    Adicionado ponto flutuante.

--- FirstCsharpProject/FirstConsole/Program.cs
+++ FirstCsharpProject/FirstConsole/Program.cs
@@ -13,6 +13,15 @@ namespace FirstConsole
             byte idade = 18;
             short bla = 123;
 
+            long bal = 123123_123; // Underline é ignorado no integer pois é só a fim de design; 
+
+            char letra = 'A';
+
+
+
+
+            float NumerComVirgula = 12.123f; // f no final especifica tipo para float somentes simples precisao (single precision)
+            float numeroComExpoente = 10e6f;
             Console.WriteLine(idade);
             Console.WriteLine(byte.MaxValue);
             Console.WriteLine(byte.MinValue);
@@ -27,8 +36,17 @@ namespace FirstConsole
             Console.WriteLine(Int16.MinValue);
             Console.WriteLine(long.MaxValue);
             Console.WriteLine(ulong.MaxValue);
+
+            Console.WriteLine($" A letra {letra} em inteiro é: {(int)letra}" ); 
             // U remove sinal.
+
+
+            Console.WriteLine(float.MinValue);
+            Console.WriteLine(float.MaxValue);
+            Console.WriteLine(numeroComExpoente);
+            Console.WriteLine(NumerComVirgula);
             Console.Read();
+
         }
     }
 }
