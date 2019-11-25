bfba06d2a3d425e4985d100b9b92e4bf7550e890
Gabriel Hillesheim da Silva <gabriel.hillesheim@philips.com>
2019-11-25 20:22:14 -0300

    Conhecendo os tipos de dados do C#

---
+++ FirstCsharpProject/FirstConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte idade = 18;
+            short bla = 123;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(byte.MaxValue);
+            Console.WriteLine(byte.MinValue);
+            Console.WriteLine(sbyte.MinValue);
+
+            // com sinal somente para byte usa s 'sbyte'
+            Console.WriteLine(short.MinValue);
+            Console.WriteLine(short.MaxValue);
+
+            Console.WriteLine(ushort.MaxValue);
+            Console.WriteLine(Int16.MaxValue);
+            Console.WriteLine(Int16.MinValue);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(ulong.MaxValue);
+            // U remove sinal.
+            Console.Read();
+        }
+    }
+}
