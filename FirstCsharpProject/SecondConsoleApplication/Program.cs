d25f72caa54453753a4304de42ece5bbd51ad9ff
Gabriel Hillesheim da Silva <gabriel.hillesheim@philips.com>
2019-11-27 18:55:39 -0300

    Inserido  novo projeto e adicionado novos itens como booleanconditions and ifs =)

---
+++ FirstCsharpProject/SecondConsoleApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float salario = 100.49f;
+            float aumento = 50.51f;
+
+            float salarioComAumento = 151.00f;
+            // problemas com arredondamento com  tipo de variavel float;
+            Console.WriteLine($"salario: {salario}, aumento: {aumento}  e salario com aumento : {salarioComAumento}");
+            Console.WriteLine(salario + aumento);
+            Console.WriteLine((salario + aumento) == salarioComAumento);
+
+            bool validacao = true;
+            bool resultado = (10 % 2 == 1); // sobra da divisão. w
+            Console.WriteLine(resultado);
+
+            if (resultado == validacao)
+            {
+                Console.WriteLine("bla bla bla");
+            }
+            else
+            {
+                Console.WriteLine(" xablau !! ");
+            }
+
+            Console.Read();
+            
+
+        }
+    }
+}
