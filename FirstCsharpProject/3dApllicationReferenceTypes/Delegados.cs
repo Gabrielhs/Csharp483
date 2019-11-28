9c4e8c0077078bcaca389d37ca0dd323fe399129
Gabriel Hillesheim da Silva <gabriel.hillesheim@philips.com>
2019-11-27 22:23:25 -0300

    Inserido a delegates;

---
+++ FirstCsharpProject/3dApllicationReferenceTypes/Delegados.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3dApllicationReferenceTypes
+{
+    class Delegados
+    {
+        public void Executar()
+        {
+            Multiplicacao conta = Triplicar;
+            Console.WriteLine(conta(10));
+
+            int Duplicar(int numero)
+            {
+                return numero * 2;
+            }
+
+            int Triplicar(int numero)
+            {
+                return numero * 3;
+            }
+
+            int Quadrado(int numero)
+            {
+                return numero * numero;
+            }
+        }
+    }
+    delegate int Multiplicacao(int numero);
+
+    
+
+    class Teste
+    {
+        Multiplicacao conta;
+        public Teste()
+        {
+           
+
+            conta = Triplicar;
+        }
+
+
+        int Duplicar(int numero)
+        {
+            return numero * 2;
+        }
+
+        int Triplicar(int numero)
+        {
+            return numero * 3;
+        }
+
+        int Qadriplicar(int numero)
+        {
+            return numero * 4;
+        }
+    }
+}
--- FirstCsharpProject/3dApllicationReferenceTypes/Program.cs
+++ FirstCsharpProject/3dApllicationReferenceTypes/Program.cs
@@ -10,6 +10,27 @@ namespace _3dApllicationReferenceTypes
     {
         static void Main(string[] args)
         {
+
+            ClassesObject MyClass = new ClassesObject();
+            ClassesObject Myclass2 = new ClassesObject();
+            ClassesObject MyClass3 = new ClassesObject(){Idade = 14, Nome = "Bla"};
+            MyClass.Idade = 1;
+            MyClass.Nome = "PowerGirl";
+
+            Myclass2 = MyClass;
+
+            Myclass2.Idade = 3;
+            Console.WriteLine(Myclass2.Idade);
+            Console.WriteLine(Myclass2.Nome);
+            Console.WriteLine(MyClass.Idade);
+            Console.WriteLine(MyClass.Nome);
+            Console.WriteLine(MyClass3.Idade);
+            Console.WriteLine(MyClass3.Nome);
+            //Console.WriteLine(Myclass2.GetType().get);
+
+
+
+            Console.Read();
         }
     }
 }
