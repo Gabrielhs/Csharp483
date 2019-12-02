4c73c8a29442b77f8443dc7640e361e7f5aef45a
Gabriel Hillesheim da Silva <gabriel.hillesheim@philips.com>
2019-12-02 18:59:26 -0300

    Added new class library and added new classes to it. Added a database.

---
+++ FirstCsharpProject/CSharp483.Dados/Base.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp483.Dados
+{
+    public class Base
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ FirstCsharpProject/CSharp483.Dados/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp483.Dados
+{
+    public class Pessoa : Base
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public byte Idade { get; set; } 
+
+
+    }
+}
